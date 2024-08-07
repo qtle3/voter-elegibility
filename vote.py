2635903eb5f8b02a7d0c722c96dbc653cda93154
Quocviet Le <quocvietlet@gmail.com>
2024-08-06 21:07:50 -0500

    updated format

--- vote.py
+++ vote.py
@@ -1,20 +1,27 @@
 # A program that determines eligibility to vote
 # in the US elections.
 
-def main() :
-    print("This program will determine if you are eligible to vote in the US elections.")
+
+def main():
+    print(
+        "This program will determine if you are eligible to vote in the US elections."
+    )
     us = input("Are you a US citizen: yes/no? ")
-    if us=="no" :
+    if us == "no":
         print("You cannot vote.")
-    else :
-        age = float(input("What is your age? "))
-        if age < 18 :
-            print("You cannot vote.")
-        else :
-            state = input("Do you meet your state’s residency requirements: yes/no? ")
-            if state=="no" :
-                print("You cannot vote.")
-            else :
-                print("You can vote!")
+        return
+
+    age = float(input("What is your age? "))
+    if age < 18:
+        print("You cannot vote.")
+        return
+
+    state = input("Do you meet your state’s residency requirements: yes/no? ")
+    if state == "no":
+        print("You cannot vote.")
+        return
+
+    print("You can vote!")
+
 
 main()
